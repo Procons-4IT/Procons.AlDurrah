90b4e969070e3107f7ced535ff9666891ef595a2
Ryan Zebian <ryan.zebian@outlook.com>
2017-11-04 06:58:40 -0700

    fix bug issue and api integration

--- Procons.Durrah.Api/ClientApp/app/worker-managment/view-profiles/view-profiles.component.ts
+++ Procons.Durrah.Api/ClientApp/app/worker-managment/view-profiles/view-profiles.component.ts
@@ -1,19 +1,27 @@
 import { Component, Input, EventEmitter, Output } from '@angular/core';
 
 import { Worker } from "../../Models/Worker";
+import * as moment from 'moment';
+import { MomentDatePipe } from '../../moment-date.pipe';
+import { LanguageConvertPipe } from '../../language-convert.pipe';
 
 @Component({
     selector: "view-profiles",
     templateUrl: "./view-profiles.component.html",
-    styles: ["./view-profiles.component.css"]
+    styles: ["./view-profiles.component.css"],
+    providers: [MomentDatePipe,LanguageConvertPipe]
 })
 export class ViewProfilesComponent {
     @Input() workers: Worker[]
     @Output() onBack = new EventEmitter<any>();
 
-    constructor() {}
+    languages: string = "";
+    constructor() { }
 
     back() {
         this.onBack.emit();
     }
+
+
+
 }
